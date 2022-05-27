f4e33126a06d68a04cc08926d0a6e09d8b52028f
sushil-edu <sushil.xperiatech@gmail.com>
2022-05-27 17:09:32 +0530

    bug bix

--- rfscanlib/src/main/java/com/example/rfscanlib/RFScan.kt
+++ rfscanlib/src/main/java/com/example/rfscanlib/RFScan.kt
@@ -7,11 +7,25 @@ import android.telephony.CellInfoLte
 import android.telephony.TelephonyManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.rfscanlib.model.RFModel
+import java.time.LocalDateTime
 import java.util.*
 
 class RFScan {
 
-    private lateinit var rfModel: RFModel
+    private lateinit var carrierName: String
+    private  var isHomeNetwork: Boolean = false
+    private var rsrp: Double=0.0
+    private var rsrq: Double=0.0
+    private var sinr: Long=0
+    private var pci: Int=0
+    private lateinit var networkType: String
+    private lateinit var lteBand: String
+    private lateinit var longitude: String
+    private lateinit var latitude: String
+    private var timestamp: Long=0
+    private var localTime: String=""
+    private var timeZone: String=""
+
 
     @SuppressLint("MissingPermission")
     fun getRFInfo(context: Context): RFModel {
@@ -24,11 +38,11 @@ class RFScan {
                 when (info) {
                     is CellInfoGsm -> {
                         val gsm = info.cellSignalStrength
-                        rfModel.rsrp = 0.0
-                        rfModel.rsrq = 0.0
-                        rfModel.sinr = 0.0
-                        rfModel.lteBand = gsm.level.toString()
-                        rfModel.pci = 0
+                       rsrp = 0.0
+                        rsrq = 0.0
+                        sinr = 0
+                      lteBand = gsm.level.toString()
+                       pci = 0
                     }
                     /*is CellInfoCdma -> {
                         val cdma = info.cellSignalStrength.cdmaDbm
@@ -36,11 +50,11 @@ class RFScan {
 
                     is CellInfoLte -> {
                         val lte = info.cellSignalStrength
-                        rfModel.rsrp = lte.rsrp.toDouble()
-                        rfModel.rsrq = lte.rsrq.toDouble()
-                        rfModel.sinr = lte.rssnr.toDouble()
-                        rfModel.lteBand = lte.level.toString()
-                        rfModel.pci = info.cellIdentity.pci
+                        rsrp = lte.rsrp.toDouble()
+                        rsrq = lte.rsrq.toDouble()
+                        sinr = lte.rssnr.toLong()
+                        lteBand = lte.level.toString()
+                        pci = info.cellIdentity.pci
 
                     }
                     else -> {
@@ -49,9 +63,10 @@ class RFScan {
                 }
                 break
             }
-            rfModel.timestamp = Calendar.getInstance().timeInMillis
-
-            return rfModel
+            timestamp = Calendar.getInstance().timeInMillis
+            localTime = LocalDateTime.now().toString()
+            timeZone =  Calendar.getInstance().time.toString().split(" ")[4]
+            return RFModel(carrierName, isHomeNetwork,rsrp, rsrq, sinr, pci, networkType, lteBand,longitude, latitude, timestamp, localTime, timeZone )
         } catch (e: Exception) {
             throw e
         }
